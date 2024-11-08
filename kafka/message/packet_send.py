8c7d247fe67a23fd2d8e7ded48c18bf52b591801
Borja <bdvazquez17@esei.uvigo.es>
2024-11-08 20:28:46 +0100

    packet_type

--- kafka/message/packet_send.py
+++ kafka/message/packet_send.py
@@ -2,12 +2,19 @@ import json
 
 class PacketSend:
 
+    types = {0: "motionPacketData",
+                   2: "lapDataPacketData",
+                   6: "carTelemetryPacketData",
+                   13: "motionExPacketData"}
+
     def __init__(self, packet):
         self.id = packet.m_header.m_packetId
+        self.packet_type = self.types.get(self.id)
         self.data = packet
 
     def to_json(self):
         return json.dumps({
             'id': self.id,
+            'packetType': self.packet_type,
             'data': self.data.to_json()
         })
